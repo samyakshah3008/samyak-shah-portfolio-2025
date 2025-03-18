c884ec324b6090e4250ef49b12c91e2d9b91cace
Samyak Shah <samyakshah3008@gmail.com>
2025-03-19 00:29:42 +0530

    feat - initialize repo

---
+++ src/constants/socials.tsx
@@ -0,0 +1,25 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+
+// import Peerlist from "../../public/images/logos/peerlist.png";
+
+export const socials = [
+  {
+    href: "https://x.com/SamyakShah_18",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://www.linkedin.com/in/samyakshah3008",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://peerlist.io/samyakshah",
+    label: "Peerlist",
+    icon: IconBrandYoutube,
+  },
+];
